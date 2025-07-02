6fe7abbc5561c8e19989a05a398ae69c9f91c233
Tomas <tomas485@gmail.com>
2025-07-02 19:14:58 +0300

    shell scripts

---
+++ RESET.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+read -p "Press enter to continue"
+echo "Reseting started"
+git reset --hard
---
+++ RESET_AND_UPDATE.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+read -p "Press enter to continue"
+echo "Reseting started"
+git reset --hard
+echo "Updating"
+git pull
